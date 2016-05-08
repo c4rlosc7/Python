a88bb4cea90f77383cd808628e5203a54dadd868
c4rlosc7 <c4rlosc7@gmail.com>
2016-05-08 14:57:57 -0500

    Start PyQt

---
+++ PyQt/absolute_positioning.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt4 tutorial 
+
+This example shows three labels on a window
+using absolute positioning. 
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: October 2011
+"""
+
+import sys
+from PyQt4.QtGui import *
+
+class Window(QWidget):
+    
+    def __init__(self):
+        super(Window, self).__init__()
+        
+        self.startUI()
+        
+    def startUI(self):
+        
+        label1 = QLabel('Label 1', self)
+        label1.move(10, 10)
+
+        label2 = QLabel('Label 2', self)
+        label2.move(50, 50)
+        
+        label3 = QLabel('Label 3', self)
+        label3.move(90, 90)        
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Absolute positioning')    
+        self.show()
+        
+def main():
+    
+    app = QApplication(sys.argv)
+    ex = Window()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
