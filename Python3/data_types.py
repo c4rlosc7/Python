0b13225865170372f6e5e755c9953b22eff11d5f
c4rlosc7 <camartinez7@hotmail.com>
2024-06-06 20:18:34 -0500

    feat: hello_world, data_type

---
+++ Python3/data_types.py
@@ -0,0 +1,23 @@
+# Type of 10 - INT
+print(type(10))
+
+# Type of 3.1415 - FLOAT
+print(type(3.1415))
+
+# Type of "Hello!!!" -  STR
+print(type("Hello!!!"))
+
+# Type of True - BOOL
+print(type(True))
+
+# Convert True to INT --> (1)
+print(int(True))
+
+# Convert False to INT --> (0)
+print(int(False))
+
+# Convert 1 to boolean - True
+print(bool(1))
+
+# Convert 0 to boolean - False
+print(bool(0))
---
+++ Python3/hello_world.py
@@ -0,0 +1,7 @@
+import sys
+
+# Print message Hello!!!
+print('Hello!!!')
+
+# Print version of Python
+print(sys.version)
